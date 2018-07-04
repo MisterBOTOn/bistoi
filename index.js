b4d24fbf3a1d0fc6e626240e69788fb7f2f210fb
MisterBOTOn <39442147+MisterBOTOn@users.noreply.github.com>
2018-07-04 20:25:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "e!"
+const prefix = "l!"
 
 client.on('ready', () => {
-  client.user.setGame('e!help ', 'https://www.twitch.tv/zzstefanro321')
+  client.user.setGame('l!help ', 'https://www.twitch.tv/zzstefanro321')
 })   
 
   client.on('message', message =>{
@@ -25,9 +25,9 @@ client.on('message', message =>{
   if ( message.content.startsWith(prefix+ "help")) {
     const embed = new Discord.RichEmbed()
     .setTitle('BOT INFO')
-    .setDescription(`•prefix --> e!
+    .setDescription(`•prefix --> l!
 BOT COMMANDS
-• || e!mass || e!support || e!info || e!say || e!ping || e!serverinfo || e!restart || e!invite || e!botinfo || `)
+• || l!mass || l!support || l!info || l!say || l!ping || l!serverinfo || l!restart || l!invite || l!botinfo || `)
     .setColor('RANDOM')
    message.channel.sendEmbed(embed);
   }
@@ -91,13 +91,13 @@ client.on('message', message =>{
 if (message.content.startsWith(prefix + 'botinfo')) {
   const embed = new Discord.RichEmbed()
   embed.addField('BOT INFO')
-  embed.addField('Name', 'EliteBOT', true)
-  embed.addField('Prefix', 'e!', true)
+  embed.addField('Name', 'LightT', true)
+  embed.addField('Prefix', 'l!', true)
   embed.addField('Tag:', '6777', true)
   embed.addField('Created at:', '2018-06-13 12:52:59.660000', true)
   embed.addField('ID', '456469970600198155', true)
   embed.addField('Made with:', 'Node')
-  embed.addField('Creator', 'Unknoun & zZstefan', true)
+  embed.addField('Creator', '<@464149460503756820> = zZstefan', true)
   embed.setColor('RANDOM')
   embed.setThumbnail(message.guild.iconURL)
   message.channel.sendEmbed(embed)
@@ -123,8 +123,8 @@ client.on('message', message =>{
    
   client.on('message', message => {    
     if(message.content.startsWith('e!mass')) {
-    if(message.author.id === "405337137735663618" ||
-message.author.id === "405337137735663618"){
+    if(message.author.id === "271360404641284096" ||
+message.author.id === "271360404641284096"){
        let args = message.content.split(" ").slice(1);         
        var argresult = args.join(" ")      
 const argsresult = args.join(" ")         
@@ -136,48 +136,48 @@ member.send(reason)
 message.delete() }})}}} });
 
 client.on('message', msg => {
-    if (msg.content === 'e!invite') {
-      msg.channel.send('Invite **EliteBOT** https://discordapp.com/api/oauth2/authorize?client_id=459730941347102722&permissions=8&scope=bot  :tada:');
+    if (msg.content === 'l!invite') {
+      msg.channel.send('Invite **Light** https://discordapp.com/api/oauth2/authorize?client_id=464146143182454794&permissions=8&scope=bot  :tada:');
     }
   });
 
 client.on('message', msg => {
-    if (msg.content === 'e!support') {
-      msg.channel.send('support server ---> https://discord.gg/r5NgFGZ');
+    if (msg.content === 'l!support') {
+      msg.channel.send('**Support server ---> https://discord.gg/7uz683J**');
     }
   });
 
 client.on('message', msg => {
-    if (msg.content === 'e!info') {
-      msg.channel.send('creatorul meu este : Unknown & zZstefan');
+    if (msg.content === 'l!info') {
+      msg.channel.send('My creator is zZstefan if you want to watch on YouTube Here`s a link: ** https: //www.youtube.com/channel/UCe3cJzGyLmIvqp0fj5PHwew? View_as = subscriber **');
     }
   });
 
 client.on('guildMemberAdd', member => {
     var joinrole = member.guild.roles.find('name', '|| 𝕰lite Membru ||');
     member.addRole(joinrole);
-    let channel  = member.guild.channels.find('name', '🤗【𝖂elcome-𝕲oodbye】😢');
+    let channel  = member.guild.channels.find('name', '➤welcome-goodbye');
     let memberavatar  = member.user.avatarURL
         if (!channel) return;
         let embed = new Discord.RichEmbed()
         .setColor('#FF000')
         .setThumbnail(memberavatar)
         .addField('✘ | Name: ', `${member}`)
-        .addField('✘ | Welcome', `**Bine ai venit pe server-ul [E.W] Elite Warriors™. Speram sa te distrezi alaturi de noi. Si nu uita sa citesti :beginner:𝕽ules:beginner:-ul**`)
+        .addField('✘ | Welcome', `Bun venit pe Light Network. :tada::hugging: !`)
         .setTimestamp()
 
         channel.sendEmbed(embed);
 });
 
 client.on('guildMemberRemove', member => {
-    let channel = member.guild.channels.find('name', '🤗【𝖂elcome-𝕲oodbye】😢');
+    let channel = member.guild.channels.find('name', '➤welcome-goodbye');
     let memberavatar = member.user.avatarURL
         if (!channel) return;
         let embed = new Discord.RichEmbed()
         .setColor('#ff0000')
         .setThumbnail(memberavatar)
         .addField('✘ | Name: ', `${member}`)
-        .addField('✘ | Bye -', `**A iesit :sob:... Speram sa te mai intorci pe la noi ... Esti mereu bine venit !**`)
+        .addField('✘ | Bye -', `**A Parasit Server-ul :slight_frown:**`)
         .setTimestamp()
 
         channel.sendEmbed(embed);
